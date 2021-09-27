dc0e9bbad867874f9007b7264ad4febf0fb50633
caofeixiang_hw <caofx0418@163.com>
2021-09-28 00:56:31 +0800

    01 add basic struct define

--- 01/play.cpp
+++ 01/play.cpp
@@ -19,12 +19,135 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
+#include <stdint.h>
 #include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
 
 using namespace std;
 
+enum class TokenKind {Keyword, Identifier, StringLiteral, Seperator, Operator, Eof};
+std::unordered_map<TokenKind, std::string> tokenToString {
+    {TokenKind::Keyword, "Keyword"},
+    {TokenKind::Identifier, "Identifier"},
+    {TokenKind::StringLiteral, "StringLiteral"},
+    {TokenKind::Seperator, "Seperator"},
+    {TokenKind::Operator, "Operator"},
+    {TokenKind::Eof, "Eof"},
+};
+
+struct Token{
+    TokenKind kind;
+    std::string text;
+};
+
+inline std::string toString(uint32_t obj) {
+    return std::to_string(obj);
+}
+
+inline std::string toString(TokenKind kind) {
+    auto it = tokenToString.find(kind);
+    if (it != tokenToString.end()) {
+        return it->second;
+    }
+
+    return "Unknow";
+}
+
+ostream& operator<<(ostream& out, Token& token) {
+    out << "{ " << toString(token.kind) << " , " << token.text << " }";
+    return out;
+}
+
+vector<Token> tokenArray = {
+    {TokenKind::Keyword,      "function"},
+    {TokenKind::Identifier,   "sayHello"},
+    {TokenKind::Seperator,    "("},
+    {TokenKind::Seperator,    ")"},
+    {TokenKind::Seperator,    "{"},
+    {TokenKind::Identifier,   "println"},
+    {TokenKind::Seperator,    "("},
+    {TokenKind::StringLiteral,"Hello World!"},
+    {TokenKind::Seperator,    ")"},
+    {TokenKind::Seperator,    ";"},
+    {TokenKind::Seperator,    "}"},
+    {TokenKind::Identifier,   "sayHello"},
+    {TokenKind::Seperator,    "("},
+    {TokenKind::Seperator,    ")"},
+    {TokenKind::Seperator,    ";"},
+    {TokenKind::Eof,          ""}
+};
+
+class Tokenizer{
+private:
+    vector<Token> tokens;
+    uint32_t pos = 0;
+
+public:
+    Tokenizer(const vector<Token>& tokens) {
+        this->tokens = tokens;
+    }
+    Token next() {
+        if (this->pos <= this->tokens.size()){
+            return this->tokens[this->pos++];
+        }
+        else{
+            //如果已经到了末尾，总是返回EOF
+            return this->tokens[this->pos];
+        }
+    }
+    uint32_t position() {
+        return this->pos;
+    }
+    void traceBack(uint32_t newPos){
+        this->pos = newPos;
+    }
+};
+
+class AstNode{
+public:
+    //打印对象信息，prefix是前面填充的字符串，通常用于缩进显示
+    virtual void dump(string prefix) = 0;
+};
+
+class Statement: public AstNode{
+public:
+    bool isStatementNode(const AstNode* node) {
+        if (!node) {
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+
+    void dump(string prefix) override {
+        std::cout << (prefix+"Statement") << std::endl;
+    }
+};
+
+class Prog: public AstNode{
+public:
+    vector<Statement> stmts; //程序中可以包含多个语句
+    Prog(vector<Statement> stmts){
+        this->stmts = stmts;
+    }
+    void dump(string prefix) override {
+        std::cout << (prefix+"Prog") << std::endl;
+        std::for_each(stmts.begin(), stmts.end(), [prefix](auto& x) { x.dump(prefix+"\t");});
+    }
+};
+
 int main() {
-    cout << "Craft a language cpp" << endl;
+    //词法分析
+    auto tokenizer = new Tokenizer(tokenArray);
+    cout << "program start use tokens" << endl;
+
+    for (auto& token: tokenArray) {
+        cout << token << endl;
+    }
 
     return 0;
 }
