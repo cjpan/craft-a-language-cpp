2a2afaafdf56f7e683f6a5d6339bc6a441ee5b5e
cao_fx <caofx0418@163.com>
2021-10-07 23:14:42 +0800

    04 Variable test program

--- 04/single_source/program.cc
+++ 04/single_source/program.cc
@@ -1,10 +1,2 @@
-function sayHello() {
-    println("Hello World!");
-}
-sayHello();
-
-//1+2*3+4;
-// let myAge:number = 18;
-// myAge = myAge + 10;
-// println("myAge is: ");
-// println(myAge);
+let myAge:number = 18;
+myAge = myAge + 10;
