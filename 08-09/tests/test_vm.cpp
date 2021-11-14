95fd0defa82591e6889d36c0b19cfbe09aa8eb33
cao_fx <caofx0418@163.com>
2021-11-14 23:35:15 +0800

    08 add vm functionDecl test

--- 08-09/tests/test_vm.cpp
+++ 08-09/tests/test_vm.cpp
@@ -164,3 +164,74 @@ println(i);
     auto ret = vm.execute(*bc);
     Print("vm.execute ret: " + std::to_string(ret), Color::Yellow);
 }
+
+TEST(VM, vm_function_Decl)
+{
+    std::string expect =
+R"(Prog
+    FunctionDecl fourTimes
+        Return type: number
+            ParamList:
+                VariableDecl r(number)
+                no initialization.
+            VariableStatement
+                VariableDecl area(number)
+                    Binary:Multiply
+                        4(integer)
+                        Variable: r, resolved
+            ReturnStatement
+                Variable: area, resolved
+    ExpressionStatement
+        FunctionCall println, built-in
+            FunctionCall fourTimes, resolved
+                4(integer)
+    ReturnStatement
+)";
+
+    std::string program =
+R"(
+function fourTimes(r : number):number{
+    let area : number = 4*r;
+    return area;
+}
+
+println(fourTimes(4));
+)";
+
+    CharStream charStream(program);
+    Scanner scanner(charStream);
+
+    auto parser = Parser(scanner);
+    auto ast = parser.parseProg();
+
+    auto dumper = AstDumper();
+    dumper.visit(*ast, "");
+
+    SemanticAnalyer semanticAnalyer;
+    semanticAnalyer.execute(*ast);
+
+    dumper.clearString();
+    dumper.visit(*ast, "");
+    auto str = dumper.toString();
+    EXPECT_STREQ(expect.c_str(), str.c_str());
+
+    auto scopeDumper = ScopeDumper();
+    scopeDumper.visit(*ast, "");
+
+    EXPECT_STREQ(expect.c_str(), str.c_str());
+
+    auto interpretor = Interpretor();
+    interpretor.visit(*ast, "");
+
+    auto generator = BCGenerator();
+    auto bcModule = generator.visit(*ast, "");
+    auto bcModuleDumper = BCModuleDumper();
+
+    auto bc = std::any_cast<std::shared_ptr<BCModule>>(bcModule);
+    bcModuleDumper.dump(*bc);
+
+    auto vm = VM();
+    auto ret = vm.execute(*bc);
+    Print("vm.execute ret: " + std::to_string(ret), Color::Yellow);
+
+}
