99c5008bd64a279f26ef2b89fedccb90e079222e
cao_fx <caofx0418@163.com>
2021-12-06 00:04:52 +0800

    20 add Asm_Multi_VariableDecl test for livenessAnalyzer

--- 20/tests/test_asm_x86-64.cpp
+++ 20/tests/test_asm_x86-64.cpp
@@ -49,6 +49,57 @@ let i = 1;
     auto asmStr = compileToAsm(*ast);
 }
 
+TEST(ASM, Asm_Multi_VariableDecl)
+{
+    std::string expect =
+R"(Prog
+    VariableStatement
+        VariableDecl i(any)
+            1(integer)
+    VariableStatement
+        VariableDecl j(any)
+            2(integer)
+    VariableStatement
+        VariableDecl x(any)
+            Binary:Plus
+                Variable: i, resolved
+                Variable: j, resolved
+    ReturnStatement
+)";
+
+    std::string program = R"(
+let i = 1;
+let j = 2;
+let x = i + j;
+)";
+    Print(program, Color::Blue);
+
+    CharStream charStream(program);
+    Scanner scanner(charStream);
+
+    auto parser = Parser(scanner);
+    auto ast = parser.parseProg();
+
+    auto dumper = AstDumper();
+    dumper.visit(*ast, "");
+
+    SemanticAnalyer semanticAnalyer;
+    semanticAnalyer.execute(*ast);
+
+    dumper.clearString();
+    dumper.visit(*ast, "");
+    auto str = dumper.toString();
+    EXPECT_STREQ(expect.c_str(), str.c_str());
+
+    auto scopeDumper = ScopeDumper();
+    scopeDumper.visit(*ast, "");
+
+    auto interpretor = Interpretor();
+    interpretor.visit(*ast, "");
+
+    auto asmStr = compileToAsm(*ast);
+}
+
 TEST(ASM, Asm_FunctionCall)
 {
     std::string expect =
